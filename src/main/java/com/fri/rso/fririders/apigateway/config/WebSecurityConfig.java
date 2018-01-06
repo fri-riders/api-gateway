c56cb028292d2385ced2bb8f2f35ee7ff27b1fed
Janez Erzen <je1468@student.uni-lj.si>
2018-01-07 00:10:09 +0100

    Add basic authentication

---
+++ src/main/java/com/fri/rso/fririders/apigateway/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.fri.rso.fririders.apigateway.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private BasicAuthenticationPoint basicAuthenticationPoint;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+        http.authorizeRequests()
+            .antMatchers("/", "/home").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/login")
+            .permitAll()
+            .and()
+            .logout()
+            .permitAll();
+        http.httpBasic().authenticationEntryPoint(basicAuthenticationPoint);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("test").roles("USER");
+    }
+}
